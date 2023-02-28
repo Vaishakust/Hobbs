b36a0a18020940eb3557e493af2613762d0ede5e
Vaishak Raju(UST <188162@ust-global.com>
2023-02-28 22:17:38 +0530

    latest

--- TAF-Scripting/src/test/java/com/scripted/web/webstepdefs/NewPage.java
+++ TAF-Scripting/src/test/java/com/scripted/web/webstepdefs/NewPage.java
@@ -21,6 +21,7 @@ public class NewPage {
 
 	@FindBy(id = "store-search-input")
 	private WebElement input_q;
+	
 	@FindBy(xpath = "//*[@id=\"store-details-pane\"]/div/button")
 	private WebElement setasmyStore;
 	@FindBy(xpath = "//div[@objectspyxpath='objectspy']")
